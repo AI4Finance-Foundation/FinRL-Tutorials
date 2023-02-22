d09473ba611ae7a9f6b9822a397a1192d76b0f6f
Ming Zhu <zhumingpassional@gmail.com>
2023-02-22 16:58:36 +0800

    add mean var

--- 1-Introduction/Stock_NeurIPS2018_SB3.py
+++ 1-Introduction/Stock_NeurIPS2018_SB3.py
@@ -6,6 +6,7 @@ def main():
     # matplotlib.use('Agg')
     import datetime
 
+
     from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
     from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
     from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
@@ -50,10 +51,7 @@ def main():
                          ticker_list=config_tickers.DOW_30_TICKER).fetch_data()
     print(config_tickers.DOW_30_TICKER)
 
-    print("df.shape: ", df.shape)
-
     df.sort_values(['date', 'tic'], ignore_index=True).head()
-
     fe = FeatureEngineer(
         use_technical_indicator=True,
         tech_indicator_list=INDICATORS,
@@ -62,7 +60,6 @@ def main():
         user_defined_feature=False)
 
     processed = fe.preprocess_data(df)
-
     list_ticker = processed["tic"].unique().tolist()
     list_date = list(pd.date_range(processed['date'].min(), processed['date'].max()).astype(str))
     combination = list(itertools.product(list_date, list_ticker))
@@ -74,6 +71,8 @@ def main():
     processed_full = processed_full.fillna(0)
 
     processed_full.sort_values(['date', 'tic'], ignore_index=True).head(10)
+
+    mvo_df = processed_full.sort_values(['date', 'tic'], ignore_index=True)[['date', 'tic', 'close']]
 
     train = data_split(processed_full, TRAIN_START_DATE, TRAIN_END_DATE)
     trade = data_split(processed_full, TRADE_START_DATE, TRADE_END_DATE)
@@ -103,12 +102,14 @@ def main():
     e_train_gym = StockTradingEnv(df=train, **env_kwargs)
 
     env_train, _ = e_train_gym.get_sb_env()
+    print(type(env_train))
+
     agent = DRLAgent(env=env_train)
 
-    if_using_a2c = False
-    if_using_ddpg = False
-    if_using_ppo = False
-    if_using_td3 = False
+    if_using_a2c = True
+    if_using_ddpg = True
+    if_using_ppo = True
+    if_using_td3 = True
     if_using_sac = True
 
     agent = DRLAgent(env=env_train)
@@ -124,6 +125,7 @@ def main():
     trained_a2c = agent.train_model(model=model_a2c,
                                     tb_log_name='a2c',
                                     total_timesteps=50000) if if_using_a2c else None
+
     agent = DRLAgent(env=env_train)
     model_ddpg = agent.get_model("ddpg")
 
@@ -137,6 +139,7 @@ def main():
     trained_ddpg = agent.train_model(model=model_ddpg,
                                      tb_log_name='ddpg',
                                      total_timesteps=50000) if if_using_ddpg else None
+
     agent = DRLAgent(env=env_train)
     PPO_PARAMS = {
         "n_steps": 2048,
@@ -144,7 +147,6 @@ def main():
         "learning_rate": 0.00025,
         "batch_size": 128,
     }
-
     model_ppo = agent.get_model("ppo", model_kwargs=PPO_PARAMS)
 
     if if_using_ppo:
@@ -174,7 +176,7 @@ def main():
 
     trained_td3 = agent.train_model(model=model_td3,
                                     tb_log_name='td3',
-                                    total_timesteps=30000) if if_using_td3 else None
+                                    total_timesteps=50000) if if_using_td3 else None
 
     agent = DRLAgent(env=env_train)
     SAC_PARAMS = {
@@ -196,7 +198,7 @@ def main():
 
     trained_sac = agent.train_model(model=model_sac,
                                     tb_log_name='sac',
-                                    total_timesteps=40000) if if_using_sac else None
+                                    total_timesteps=50000) if if_using_sac else None
 
     data_risk_indicator = processed_full[
         (processed_full.date < TRAIN_END_DATE) & (processed_full.date >= TRAIN_START_DATE)]
@@ -213,38 +215,199 @@ def main():
     e_trade_gym = StockTradingEnv(df=trade, turbulence_threshold=70, risk_indicator_col='vix', **env_kwargs)
     # env_trade, obs_trade = e_trade_gym.get_sb_env()
 
-    trained_moedl = trained_sac
-    df_account_value, df_actions = DRLAgent.DRL_prediction(
+    trained_moedl = trained_a2c
+    df_account_value_a2c, df_actions_a2c = DRLAgent.DRL_prediction(
         model=trained_moedl,
         environment=e_trade_gym)
 
-    print("df_account_value.shape: ", df_account_value.shape)
+    trained_moedl = trained_ddpg
+    df_account_value_ddpg, df_actions_ddpg = DRLAgent.DRL_prediction(
+        model=trained_moedl,
+        environment=e_trade_gym)
 
-    print("==============Get Backtest Results===========")
-    now = datetime.datetime.now().strftime('%Y%m%d-%Hh%M')
+    trained_moedl = trained_ppo
+    df_account_value_ppo, df_actions_ppo = DRLAgent.DRL_prediction(
+        model=trained_moedl,
+        environment=e_trade_gym)
 
-    perf_stats_all = backtest_stats(account_value=df_account_value)
-    perf_stats_all = pd.DataFrame(perf_stats_all)
-    perf_stats_all.to_csv("./" + RESULTS_DIR + "/perf_stats_all_" + now + '.csv')
+    trained_moedl = trained_td3
+    df_account_value_td3, df_actions_td3 = DRLAgent.DRL_prediction(
+        model=trained_moedl,
+        environment=e_trade_gym)
 
-    # baseline stats
-    print("==============Get Baseline Stats===========")
-    baseline_df = get_baseline(
-        ticker="^DJI",
-        start=df_account_value.loc[0, 'date'],
-        end=df_account_value.loc[len(df_account_value) - 1, 'date'])
+    trained_moedl = trained_sac
+    df_account_value_sac, df_actions_sac = DRLAgent.DRL_prediction(
+        model=trained_moedl,
+        environment=e_trade_gym)
 
-    stats = backtest_stats(baseline_df, value_col_name='close')
+    fst = mvo_df
+    fst = fst.iloc[0 * 29:0 * 29 + 29, :]
+    tic = fst['tic'].tolist()
 
-    print("==============Compare to DJIA===========")
-    # S&P 500: ^GSPC
-    # Dow Jones Index: ^DJI
-    # NASDAQ 100: ^NDX
-    backtest_plot(df_account_value,
-                  baseline_ticker='^DJI',
-                  baseline_start=df_account_value.loc[0, 'date'],
-                  baseline_end=df_account_value.loc[len(df_account_value) - 1, 'date'])
+    mvo = pd.DataFrame()
 
+    for k in range(len(tic)):
+        mvo[tic[k]] = 0
+
+    for i in range(mvo_df.shape[0] // 29):
+        n = mvo_df
+        n = n.iloc[i * 29:i * 29 + 29, :]
+        date = n['date'][i * 29]
+        mvo.loc[date] = n['close'].tolist()
+
+    from scipy import optimize
+    from scipy.optimize import linprog
+
+    # function obtains maximal return portfolio using linear programming
+
+    def MaximizeReturns(MeanReturns, PortfolioSize):
+
+        # dependencies
+
+        c = (np.multiply(-1, MeanReturns))
+        A = np.ones([PortfolioSize, 1]).T
+        b = [1]
+        res = linprog(c, A_ub=A, b_ub=b, bounds=(0, 1), method='simplex')
+
+        return res
+
+    def MinimizeRisk(CovarReturns, PortfolioSize):
+
+        def f(x, CovarReturns):
+            func = np.matmul(np.matmul(x, CovarReturns), x.T)
+            return func
+
+        def constraintEq(x):
+            A = np.ones(x.shape)
+            b = 1
+            constraintVal = np.matmul(A, x.T) - b
+            return constraintVal
+
+        xinit = np.repeat(0.1, PortfolioSize)
+        cons = ({'type': 'eq', 'fun': constraintEq})
+        lb = 0
+        ub = 1
+        bnds = tuple([(lb, ub) for x in xinit])
+
+        opt = optimize.minimize(f, x0=xinit, args=(CovarReturns), bounds=bnds, \
+                                constraints=cons, tol=10 ** -3)
+
+        return opt
+
+    def MinimizeRiskConstr(MeanReturns, CovarReturns, PortfolioSize, R):
+
+        def f(x, CovarReturns):
+            func = np.matmul(np.matmul(x, CovarReturns), x.T)
+            return func
+
+        def constraintEq(x):
+            AEq = np.ones(x.shape)
+            bEq = 1
+            EqconstraintVal = np.matmul(AEq, x.T) - bEq
+            return EqconstraintVal
+
+        def constraintIneq(x, MeanReturns, R):
+            AIneq = np.array(MeanReturns)
+            bIneq = R
+            IneqconstraintVal = np.matmul(AIneq, x.T) - bIneq
+            return IneqconstraintVal
+
+        xinit = np.repeat(0.1, PortfolioSize)
+        cons = ({'type': 'eq', 'fun': constraintEq},
+                {'type': 'ineq', 'fun': constraintIneq, 'args': (MeanReturns, R)})
+        lb = 0
+        ub = 1
+        bnds = tuple([(lb, ub) for x in xinit])
+
+        opt = optimize.minimize(f, args=(CovarReturns), method='trust-constr', \
+                                x0=xinit, bounds=bnds, constraints=cons, tol=10 ** -3)
+
+        return opt
+
+    def StockReturnsComputing(StockPrice, Rows, Columns):
+        import numpy as np
+        StockReturn = np.zeros([Rows - 1, Columns])
+        for j in range(Columns):  # j: Assets
+            for i in range(Rows - 1):  # i: Daily Prices
+                StockReturn[i, j] = ((StockPrice[i + 1, j] - StockPrice[i, j]) / StockPrice[i, j]) * 100
+
+        return StockReturn
+
+    # Obtain optimal portfolio sets that maximize return and minimize risk
+
+    # Dependencies
+    import numpy as np
+    import pandas as pd
+
+    # input k-portfolio 1 dataset comprising 15 stocks
+    # StockFileName = './DJIA_Apr112014_Apr112019_kpf1.csv'
+
+    Rows = 1259  # excluding header
+    Columns = 15  # excluding date
+    portfolioSize = 29  # set portfolio size
+
+    # read stock prices in a dataframe
+    # df = pd.read_csv(StockFileName,  nrows= Rows)
+
+    # extract asset labels
+    # assetLabels = df.columns[1:Columns+1].tolist()
+    # print(assetLabels)
+
+    # extract asset prices
+    # StockData = df.iloc[0:, 1:]
+    StockData = mvo.head(mvo.shape[0] - 336)
+    TradeData = mvo.tail(336)
+    # df.head()
+    TradeData.to_numpy()
+
+    # compute asset returns
+    arStockPrices = np.asarray(StockData)
+    [Rows, Cols] = arStockPrices.shape
+    arReturns = StockReturnsComputing(arStockPrices, Rows, Cols)
+
+    # compute mean returns and variance covariance matrix of returns
+    meanReturns = np.mean(arReturns, axis=0)
+    covReturns = np.cov(arReturns, rowvar=False)
+
+    # set precision for printing results
+    np.set_printoptions(precision=3, suppress=True)
+
+    # display mean returns and variance-covariance matrix of returns
+    print('Mean returns of assets in k-portfolio 1\n', meanReturns)
+    print('Variance-Covariance matrix of returns\n', covReturns)
+
+    from pypfopt.efficient_frontier import EfficientFrontier
+
+    ef_mean = EfficientFrontier(meanReturns, covReturns, weight_bounds=(0, 0.5))
+    raw_weights_mean = ef_mean.max_sharpe()
+    cleaned_weights_mean = ef_mean.clean_weights()
+    mvo_weights = np.array([1000000 * cleaned_weights_mean[i] for i in range(29)])
+    mvo_weights
+
+    LastPrice = np.array([1 / p for p in StockData.tail(1).to_numpy()[0]])
+    Initial_Portfolio = np.multiply(mvo_weights, LastPrice)
+    Initial_Portfolio
+
+    Portfolio_Assets = TradeData @ Initial_Portfolio
+    MVO_result = pd.DataFrame(Portfolio_Assets, columns=["Mean Var"])
+    MVO_result
+
+    df_result_a2c = df_account_value_a2c.set_index(df_account_value_a2c.columns[0])
+    df_result_ddpg = df_account_value_ddpg.set_index(df_account_value_ddpg.columns[0])
+    df_result_td3 = df_account_value_td3.set_index(df_account_value_td3.columns[0])
+    df_result_ppo = df_account_value_ppo.set_index(df_account_value_ppo.columns[0])
+    df_result_sac = df_account_value_sac.set_index(df_account_value_sac.columns[0])
+
+    result = pd.merge(df_result_a2c, df_result_ddpg, left_index=True, right_index=True)
+    result = pd.merge(result, df_result_td3, left_index=True, right_index=True)
+    result = pd.merge(result, df_result_ppo, left_index=True, right_index=True)
+    result = pd.merge(result, df_result_sac, left_index=True, right_index=True)
+    result = pd.merge(result, MVO_result, left_index=True, right_index=True)
+    result.columns = ['a2c', 'ddpg', 'td3', 'ppo', 'sac', 'mean var']
+
+    plt.rcParams["figure.figsize"] = (15, 5)
+    plt.figure();
+    result.plot();
 
 
 if __name__ == '__main__':
